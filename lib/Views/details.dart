5ba1924f653157865a092ee1cc0f83c259345c40
Julia Pak <41366455+iNeedHelpX@users.noreply.github.com>
2021-07-23 09:45:21 -0400

    added notes to indicate what page this is for

--- lib/Views/details.dart
+++ lib/Views/details.dart
@@ -2,6 +2,7 @@ import 'package:customersmartapp/colors/colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+// the detail view page that is the by product of clicking through on a listing from the shopping page
 class DetailsView extends StatelessWidget {
   final String imgUrl;
 
