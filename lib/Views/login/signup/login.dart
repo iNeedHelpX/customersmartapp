28e3ad0b3bf1f54cd4e506941ddd72a14d1190da
Julia Pak <41366455+iNeedHelpX@users.noreply.github.com>
2021-08-02 22:47:08 -0400

    pages edit

--- lib/Views/login/signup/login.dart
+++ lib/Views/login/signup/login.dart
@@ -1,3 +1,4 @@
+import 'package:customersmartapp/colors/colors.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -10,8 +11,55 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
-    );
+    return Stack(children: [
+      Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+        margin: EdgeInsets.symmetric(vertical: 85, horizontal: 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: text2,
+          boxShadow: [
+            BoxShadow(
+              color: Theme.of(context).hintColor.withOpacity(0.2),
+              offset: Offset(0, 10),
+              blurRadius: 20,
+            )
+          ],
+        ),
+        child: Form(
+          child: Column(
+            children: [
+              SizedBox(height: 25),
+              Text(
+                "Login",
+                style: Theme.of(context).textTheme.headline2,
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                // onSaved: (input) => loginRequestModel.email = input,
+                validator: (input) =>
+                    input!.contains('@') ? "Email Id should be valid" : null,
+                decoration: new InputDecoration(
+                  hintText: "Email Address",
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color:
+                              Theme.of(context).accentColor.withOpacity(0.2))),
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Theme.of(context).accentColor)),
+                  prefixIcon: Icon(
+                    Icons.email,
+                    color: Theme.of(context).accentColor,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ]);
   }
 }
