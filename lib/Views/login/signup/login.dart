2a393d2c16a1cd1f4a7b2e833404b1f1179a608b
Julia Pak <41366455+iNeedHelpX@users.noreply.github.com>
2021-08-02 22:14:00 -0400

    widget

--- lib/Views/login/signup/login.dart
+++ lib/Views/login/signup/login.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
--- lib/Views/login/signup/signup.dart
+++ lib/Views/login/signup/signup.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+class Signup extends StatefulWidget {
+  Signup({Key? key}) : super(key: key);
+
+  @override
+  _SignupState createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
