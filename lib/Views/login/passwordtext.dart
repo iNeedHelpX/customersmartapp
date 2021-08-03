024c83fb6f727c2ad020d095f880eadc0a3dd6f9
Julia Pak <41366455+iNeedHelpX@users.noreply.github.com>
2021-08-03 01:24:33 -0400

    password

---
+++ lib/Views/login/passwordtext.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PasswordText extends StatefulWidget {
+  final String passwordQue;
+  PasswordText({required this.passwordQue});
+
+  @override
+  _PasswordTextState createState() => _PasswordTextState();
+}
+
+class _PasswordTextState extends State<PasswordText> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
