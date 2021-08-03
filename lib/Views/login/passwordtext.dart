7a78afa8547570a271a4039809923a8306c9b9e4
Julia Pak <41366455+iNeedHelpX@users.noreply.github.com>
2021-08-03 01:37:03 -0400

    password

--- lib/Views/login/passwordtext.dart
+++ lib/Views/login/passwordtext.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class PasswordText extends StatefulWidget {
   final String passwordQue;
+
   PasswordText({required this.passwordQue});
 
   @override
@@ -9,10 +10,38 @@ class PasswordText extends StatefulWidget {
 }
 
 class _PasswordTextState extends State<PasswordText> {
+  bool hidePassword = true;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return TextFormField(
+      style: TextStyle(color: Theme.of(context).accentColor),
+      keyboardType: TextInputType.text,
+      //onSaved: (input) => loginRequestModel.password = input,
+      validator: (input) => input!.length < 3
+          ? "Password should be more than 3 characters"
+          : null,
+      obscureText: hidePassword,
+      decoration: new InputDecoration(
+        hintText: "Password",
+        enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+                color: Theme.of(context).accentColor.withOpacity(0.2))),
+        focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Theme.of(context).accentColor)),
+        prefixIcon: Icon(
+          Icons.lock,
+          color: Theme.of(context).accentColor,
+        ),
+        suffixIcon: IconButton(
+          onPressed: () {
+            setState(() {
+              hidePassword = !hidePassword;
+            });
+          },
+          color: Theme.of(context).accentColor.withOpacity(0.4),
+          icon: Icon(hidePassword ? Icons.visibility_off : Icons.visibility),
+        ),
+      ),
     );
   }
 }
