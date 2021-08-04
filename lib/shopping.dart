80228549555eefd10d0b698eefe0c37a583aa4af
Julia Pak <41366455+iNeedHelpX@users.noreply.github.com>
2021-08-04 19:59:18 -0400

    update

--- lib/shopping.dart
+++ lib/shopping.dart
@@ -26,33 +26,12 @@ class _ShoppingState extends State<Shopping> {
           ),
           CustomContainer(
               imgurl:
-                  'https://cdn.thisiswhyimbroke.com/images/color-changing-led-eyelashes-cylaplex1.gif',
-              name: 'LED lashes',
-              deliverfee: '\$20'),
-          SizedBox(
-            height: 20,
-          ),
-          CustomContainer(
-              imgurl:
-                  'https://cdn.shopify.com/s/files/1/0379/3698/8205/files/ezgif-4-1eda651fbaba_480x480.gif?v=1597420417',
-              name: 'Glow Face Mask',
-              deliverfee: "\$25"),
-          SizedBox(
-            height: 20,
-          ),
-          CustomContainer(
-              imgurl:
                   'https://sc04.alicdn.com/kf/Hea2914ce4f9f4d4daf2be954652a4839g.jpg',
               name: "Menstrual Cup",
               deliverfee: "\$10"),
           SizedBox(
             height: 20,
           ),
-          CustomContainer(
-              imgurl:
-                  'https://sc04.alicdn.com/kf/H80605f672e5e46aa84fb39309b1f5e52L.jpg',
-              name: 'T-Shirt',
-              deliverfee: "\$25")
         ],
       ),
     ));
