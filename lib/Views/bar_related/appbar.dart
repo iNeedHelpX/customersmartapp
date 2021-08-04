6c844a2ec452c7b36b053ab21a8a02f28d77c632
Julia Pak <41366455+iNeedHelpX@users.noreply.github.com>
2021-08-04 19:52:12 -0400

    size

--- lib/Views/bar_related/appbar.dart
+++ lib/Views/bar_related/appbar.dart
@@ -87,7 +87,7 @@ AppBar pinkAppBar(BuildContext context) {
               fontFamily: 'asian',
               color: textbg,
               fontWeight: FontWeight.w500,
-              fontSize: 50,
+              fontSize: 45,
               shadows: [
                 Shadow(
                   offset: Offset(6.0, 8.0),
