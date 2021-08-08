6b64959ed62a6877b2d1fa0d37863392d95a4537
Julia Pak <41366455+iNeedHelpX@users.noreply.github.com>
2021-08-08 11:36:57 -0400

    needs fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:customersmartapp/enterapp.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() {
   //set portrait up orientation. Not able to rotate.
