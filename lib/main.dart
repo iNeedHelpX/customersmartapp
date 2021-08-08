6fa02585677c188046091f7dd4407f1bb99ce268
Julia Pak <41366455+iNeedHelpX@users.noreply.github.com>
2021-08-08 11:21:08 -0400

    portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:customersmartapp/enterapp.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  //set portrait up orientation. Not able to rotate.
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(EnterPoint());
 }
