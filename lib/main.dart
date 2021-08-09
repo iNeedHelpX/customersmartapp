ce2458f3960912c9c98021d73153b7472c34ed4e
Julia Pak <41366455+iNeedHelpX@users.noreply.github.com>
2021-08-08 21:45:38 -0400

    change to app enter

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:customersmartapp/enterapp.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   //set portrait up orientation. Not able to rotate.
   // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  runApp(EnterPoint());
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(EnterPoint());
+  });
 }
