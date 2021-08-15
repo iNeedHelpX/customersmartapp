9cc9c7daf5b4eaca20153cb8972d95f5890fc501
Julia Pak <41366455+iNeedHelpX@users.noreply.github.com>
2021-08-15 02:41:25 -0400

    CODE

--- contracts/logistic.sol
+++ contracts/logistic.sol
@@ -3,13 +3,17 @@ pragma solidity >=0.4.22 <0.9.0;
 
 //assign delivery of item ordered by customer
 contract Logistics {
+    //DECLARATION
     struct PackageDetails {
-        bool isUidGenerated;
-        uint256 itemID;
-        string itemName;
-        uint256 orderStatus;
-        string transitStatus;
         address customer;
+        bool isUidGenerated;
+        //256 bit integer values
+        uint256 itemID;
+        uint256 orderStatus;
         uint256 orderTime;
+        //name and status
+        string itemName;
+        string transitStatus;
     }
+    //END DECLARATION
 }
