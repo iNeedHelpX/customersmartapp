17f09ed8b7da3474b2fb6c8ed46eb17de54aa7f3
Julia Pak <41366455+iNeedHelpX@users.noreply.github.com>
2021-08-15 01:37:08 -0400

    logistics

--- contracts/logistic.sol
+++ contracts/logistic.sol
@@ -3,5 +3,13 @@ pragma solidity >=0.4.22 <0.9.0;
 
 //assign delivery of item ordered by customer
 contract Logistics {
-
+    struct package {
+        bool isUidGenerated;
+        uint256 itemID;
+        string itemName;
+        uint256 orderStatus;
+        string transitStatus;
+        address customer;
+        uint256 orderTime;
+    }
 }
