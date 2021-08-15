ed0763580ff738f8793264cff3fe1ed66aa25c3f
Julia Pak <41366455+iNeedHelpX@users.noreply.github.com>
2021-08-15 06:46:23 -0400

    voter

--- contracts/logistic.sol
+++ contracts/logistic.sol
@@ -1,40 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity >= 0.4.20.0 <0.9.0;
 
-// //assign delivery of item ordered by customer
-// contract Logistics {
-//     //DECLARATION
-//     struct packagedetails {
-//         address customer;
-//         bool isUidGenerated;
-//         //256 bit integer values
-//         uint256 itemID;
-//         uint256 orderStatus; //1 = ordered; 2 = in transit; 3 = delivered; 4 = cancelled
-//         uint256 orderTime;
-//         //name and status
-//         string itemName;
-//         string transitStatus;
-//     }
-//     mapping(address => packagedetails) public packageMapping;
-
-//     //END DECLARATION
-//     function OrderItem(uint256 _itemID, string _itemName)
-//         public
-//         pure
-//         returns (address)
-//     {
-//         address uniqueId = address(sha256(msg.sender, now));
-//         packageMapping[uniqueId].isUidGenerated = true;
-//         packageMapping[uniqueId].itemID = _itemID;
-//         packageMapping[uniqueId].itemName = _itemName;
-//         packageMapping[uniqueId]
-//             .transitStatus = "package ordered! Processing..";
-//         packageMapping[uniqueId].orderStatus = 1;
-//         packageMapping[uniqueId].customer = msg.sender;
-//         packageMapping[uniqueId].orderTime = now;
-//         return uniqueId;
-//     }
-// }
+//not logistic yet
+/// @title Voting with delegation.
 contract Ballot {
     // This declares a new complex type which will
     // be used for variables later.
