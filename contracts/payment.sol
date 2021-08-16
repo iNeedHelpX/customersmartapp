df024afae94b06557376946aef852956cdf78fa4
Julia Pak <41366455+iNeedHelpX@users.noreply.github.com>
2021-08-16 08:00:16 -0400

    eth

--- contracts/payment.sol
+++ contracts/payment.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-//payments contract
-contract Payment {
-
+//payments contract to accept ether
+contract Wallet {
+    event Deposit(address sender, uint256 amount, uint256 balance);
+    event Withdraw(uint256 amount, uint256 balance);
+    event Transfer(address to, uint256 amount, uint256 balance);
 }
