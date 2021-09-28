86451db848d8085eb767bf8544ba0cf2e133a4d3
Julia Pak <41366455+iNeedHelpX@users.noreply.github.com>
2021-09-28 18:29:59 -0400

    change

--- lib/api/google_signin_api.dart
+++ lib/api/google_signin_api.dart
@@ -2,5 +2,5 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleSignInApi {
   static final _googleSignIn = GoogleSignIn();
-  static Future login() => _googleSignIn.signIn();
+  static Future<GoogleSignInAccount?> login() => _googleSignIn.signIn();
 }
